e9c0714dfd460ca4896f2f96349348601ad7a3c3
chen2438 <2363822647@qq.com>
2022-11-25 23:12:09 +0800

    Update login.php

--- login/login.php
+++ login/login.php
@@ -1,10 +1,12 @@
 <?php
-$arr = $_POST['arr'];
-if(empty($arr)){
-    echo "Arguments are empty";
+$user_name = $_POST['user_name'];
+$user_phone = $_POST['user_phone'];
+$values = $user_name + $user_phone;
+if(empty($values)){
+    echo "values is empty";
 }
 else{
-    $command = "./login " . escapeshellcmd($arr);
+    $command = "./login " . escapeshellcmd($values);
     $return = passthru($command);
 }
 ?>

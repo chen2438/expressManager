947ccad12f68805600fabd2be33157f1d99bee51
chen2438 <2363822647@qq.com>
2022-11-25 21:15:23 +0800

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -7,12 +7,11 @@ int main() {
     MyDB db;
     //连接数据库
     db.initDB("localhost", "root", "Chen2438!", "test");
-    exit(0);
     //将用户信息添加到数据库
-    db.exeSQL("INSERT accounts values('fengxin','123');");
-    db.exeSQL("INSERT accounts values('axin','456');");
+    db.exeSQL("INSERT user values('10000','123456','13456982338');");
+    // db.exeSQL("INSERT accounts values('axin','456');");
     //将所有用户信息读出，并输出。
-    db.exeSQL("SELECT * from accounts;");
+    db.exeSQL("SELECT * from user;");
 
     return 0;
 }

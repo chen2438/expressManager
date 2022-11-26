f9e22ab1ce5ad0f591b8530f3698e88f024c7268
chen2438 <2363822647@qq.com>
2022-11-26 23:06:23 +0800

    Update logIn.php

--- php/logIn.php
+++ php/logIn.php
@@ -12,11 +12,11 @@ for ($i = 0; $i < count($output); $i++) {
     echo '<br>';
 }
 if ($output[count($output) - 1] == "Login successful!") {
-    // header("refresh: 5;url=../recepient?phone=" . $phone . ".html");
+    header("refresh: 5;url=../recepient?phone=" . $phone . ".html");
     echo "PHP: Please wait 3 seconds to refresh.";
     echo '<br>';
 } else {
-    // header("refresh: 5;url=../html.html");
+    header("refresh: 5;url=../html.html");
     echo "PHP: Please wait 3 seconds to turn back.";
     echo '<br>';
 }

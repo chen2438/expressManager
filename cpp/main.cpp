b9b3c435947fa99aa06e4a3c39ddbe4de744bfc0
chen2438 <2363822647@qq.com>
2022-11-26 15:43:53 +0800

    Update main.cpp

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -6,11 +6,7 @@ using namespace std;
 
 int main(int argc, char* argv[]) {  //系统界面传参
     //参数：本身,执行动作，动作参数
-
-    // ofstream out("main.out", ios::out | ios::trunc);
-    // out << argv[2] << argv[3] << argv[4];
-    // out.close();
-    cout << "first" << endl;
+    cout << argv[1] << endl;
     if (argc == 1) {  //无参数传入，执行控制台UI
         // codes
     } else if (argv[1] == "signUp") {

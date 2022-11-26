64787950cd0e940556ed60bfda3e936c51e148bf
chen2438 <2363822647@qq.com>
2022-11-26 19:30:16 +0800

    upd

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,15 +1,19 @@
 #include "expressObject.cpp"
 
-// #include <iostream>
-using namespace std;
+char* Collector::adminPasswd = "admin";
 
 int main(int argc, char* argv[]) {  //系统界面传参
-    //参数：本身,执行动作，动作参数
     if (argc == 1) {
-    } else if (argv[1][0] == 's') {
+    } else if (strcmp(argv[1], "signUp") == 0) {
+        // self,action,phone,passwd,userType
         cout << "Start registration." << endl;
-        User user;
-        user.signUp(argv[2], argv[3], argv[4]);
+        if (strcmp(argv[4], "0") == 0) {
+            Recipient recipient;
+            recipient.signUp(argv[2], argv[3], "recipient");
+        } else if (strcmp(argv[4], Collector::adminPasswd) == 0) {
+            Collector collector;
+            collector.signUp(argv[2], argv[3], "collector");
+        }
         cout << "Registration successful!" << endl;
     }
     return 0;

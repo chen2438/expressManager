ee2a6a09ee9b2c0e732792c152d30ac179dd9783
chen2438 <2363822647@qq.com>
2022-11-26 15:43:06 +0800

    Update main.cpp

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -10,6 +10,7 @@ int main(int argc, char* argv[]) {  //系统界面传参
     // ofstream out("main.out", ios::out | ios::trunc);
     // out << argv[2] << argv[3] << argv[4];
     // out.close();
+    cout << "first" << endl;
     if (argc == 1) {  //无参数传入，执行控制台UI
         // codes
     } else if (argv[1] == "signUp") {

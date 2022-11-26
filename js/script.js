8c20d3ac0fd0f0563b0d6616b432972b7e9ff151
chen2438 <2363822647@qq.com>
2022-11-26 23:05:45 +0800

    upd

--- js/script.js
+++ js/script.js
@@ -1,2 +1,11 @@
-const phone = document.querySelector('.phone');
-// phone.addEventListener('click', updateName);
+function getQuery(property) {
+    var query = window.location.search.substring(1);
+    var vars = query.split("&");
+    for (var i = 0; i < vars.length; i++) {
+        var pair = vars[i].split("=");
+        if (pair[0] === property) {
+            return pair[1];
+        }
+    }
+    return -1;
+}

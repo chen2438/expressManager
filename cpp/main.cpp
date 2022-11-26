49c8e349ec5229658d1258f918b9bf78e226ad4e
chen2438 <2363822647@qq.com>
2022-11-26 15:56:26 +0800

    upd

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,16 +1,13 @@
 #include "expressObject.cpp"
 
-// #include <fstream>
 // #include <iostream>
 using namespace std;
 
-int main(int argc, string argv[]) {  //系统界面传参
+int main(int argc, char* argv[]) {  //系统界面传参
     //参数：本身,执行动作，动作参数
-    cout << (argv[1] == "signUp") << endl;
-    if (argc == 1) {  //无参数传入，执行控制台UI
-        // codes
-    } else if (argv[1] == "signUp") {
-        cout << "enter signup modle" << endl;
+    if (argc == 1) {
+    } else if (argv[1][0] == 's') {
+        // cout << "enter signup modle" << endl;
         User user;
         user.signUp(argv[2], argv[3], argv[4]);
     }

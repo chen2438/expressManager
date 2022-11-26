1f360510e3ce4761dae94a228b3d7c4502d0f458
chen2438 <2363822647@qq.com>
2022-11-26 15:29:26 +0800

    Update main.cpp

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -1,18 +1,21 @@
-#include "expressObject.cpp"
-// #include "expressObject.h"
+// #include "expressObject.cpp"
+
+#include <iostream>
+using namespace std;
 
 int main(int argc, char* argv[]) {  //系统界面传参
     //参数：本身,执行动作，动作参数
     cout << argc << endl;
+    cout << "hello" << endl;
     if (argc == 1) {  //无参数传入，执行控制台UI
         // codes
 
     } else if (argv[1] == "signUp") {
-        ofstream out("main.out", ios::out | ios::trunc);
-        out << argv[2] << argv[3] << argv[4];
-        out.close();
-        User user;
-        user.signUp(argv[2], argv[3], argv[4]);
+        // ofstream out("main.out", ios::out | ios::trunc);
+        // out << argv[2] << argv[3] << argv[4];
+        // out.close();
+        // User user;
+        // user.signUp(argv[2], argv[3], argv[4]);
     }
     return 0;
 }

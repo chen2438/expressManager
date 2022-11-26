a4fbb1dd007a989b93add4faf5309c68cc0a9f85
chen2438 <2363822647@qq.com>
2022-11-26 16:23:03 +0800

    upd

--- cpp/expressObject.cpp
+++ cpp/expressObject.cpp
@@ -2,19 +2,18 @@
 
 int User::signUp(string phone, string passwd, string userType) {
     ifstream connectDB("connectDB.txt", ios::in);  //从文件读取数据库登入信息
-    char argv[10];
+    string argv[10];
     int argc = 0;
 
-    while (connectDB >> argv) {
-        cout << argv << endl;
+    while (connectDB >> argv[argc]) {
+        cout << argv[argc] << endl;
         argc++;
     }
     cout << 1 << endl;
     connectDB.close();
     return 0;
-    MyDB db;  //建立数据库连接
-    // db.initDB(argv[0], argv[1], argv[2], argv[3]);  //
-    // host,user,passwd,dbName
+    MyDB db;                                        //建立数据库连接
+    db.initDB(argv[0], argv[1], argv[2], argv[3]);  // host,user,passwd,dbName
     string cmd =
         "insert user values(" + phone + "," + passwd + "," + userType + ");";
     db.exeSQL(cmd);

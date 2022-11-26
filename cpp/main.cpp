7591405f24e71d65c86dbcd21d479e241ca1dbfe
chen2438 <2363822647@qq.com>
2022-11-26 19:39:06 +0800

    Update main.cpp

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -8,11 +8,15 @@ int main(int argc, char* argv[]) {  //系统界面传参
         // self,action,phone,passwd,userType
         cout << "Start registration." << endl;
         if (strcmp(argv[4], "0") == 0) {
+            cout << "Register for recipient." << endl;
             Recipient recipient;
-            recipient.signUp(argv[2], argv[3], "recipient");
+            argv[4] = "recipient";
+            recipient.signUp(argv[2], argv[3], argv[4]);
         } else if (strcmp(argv[4], Collector::adminPasswd) == 0) {
+            cout << "Correct password. Register for collector." << endl;
             Collector collector;
-            collector.signUp(argv[2], argv[3], "collector");
+            argv[4] = "collector";
+            collector.signUp(argv[2], argv[3], argv[4]);
         }
         cout << "Registration successful!" << endl;
     }

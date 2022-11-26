16d95c18517042dac1d0d9788001c3e4de36e4eb
chen2438 <2363822647@qq.com>
2022-11-26 12:05:15 +0800

    Update login.php

--- login/login.php
+++ login/login.php
@@ -4,13 +4,13 @@ $user_name = $_POST['user_name'];
 $user_phone = $_POST['user_phone'];
 $values = $user_name . ' ' . $user_phone;
 if (empty($values)) {
-    echo "values is empty";
     fwrite($file, "values is empty");
 } else {
     $command = "./login " . escapeshellcmd($values);
     exec($command, $output);
-    fwrite($file, $output);
+    for ($i = 0; $i < count($output); $i++) {
+        fwrite($file, $output[$i]);
+    }
 }
-fwrite($file, "test");
 fclose($file);
 ?>

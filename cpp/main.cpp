450957700a9bebee5c2b4dee8c1c7f8bd48b5b81
chen2438 <2363822647@qq.com>
2022-11-26 15:42:18 +0800

    Update main.cpp

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -13,6 +13,7 @@ int main(int argc, char* argv[]) {  //系统界面传参
     if (argc == 1) {  //无参数传入，执行控制台UI
         // codes
     } else if (argv[1] == "signUp") {
+        cout << "enter signup modle" << endl;
         User user;
         user.signUp(argv[2], argv[3], argv[4]);
     }

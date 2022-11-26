83eb2ae2d78e853ab69b5694bd3380b0031b781c
chen2438 <2363822647@qq.com>
2022-11-26 22:20:08 +0800

    upd

--- php/logIn.php
+++ php/logIn.php
@@ -3,20 +3,20 @@ $phone = $_POST['phone'];
 $passwd = $_POST['passwd'];
 
 $values = 'logIn ' . $phone . ' ' . $passwd;
-
 $command = "../cpp/main " . escapeshellcmd($values);
 exec($command, $output);
 
-// $file = fopen("signUp.txt", "w+") or die("Unable to open file!");
 for ($i = 0; $i < count($output); $i++) {
     echo 'C++: ' . $output[$i];
     echo '<br>';
-    // fwrite($file, $output[$i] . "\n");
 }
-
-// header("refresh: 5;url=../index.html");
-echo "PHP: Please wait 3 seconds to turn back.\n";
-
-exit;
-// fclose($file);
+if ($output[count($output) - 1] == "Login successful!") {
+    // header("refresh: 5;url=../recepient?phone=" . $phone . ".html");
+    echo "PHP: Please wait 3 seconds to refresh.";
+    echo '<br>';
+} else {
+    // header("refresh: 5;url=../html.html");
+    echo "PHP: Please wait 3 seconds to turn back.";
+    echo '<br>';
+}
 ?>

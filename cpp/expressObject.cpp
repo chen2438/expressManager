b1a1ca08d3c6fe98e8b732f365bf60c3437c066c
chen2438 <2363822647@qq.com>
2022-11-26 16:44:09 +0800

    in

--- cpp/expressObject.cpp
+++ cpp/expressObject.cpp
@@ -1,7 +1,7 @@
 #include "expressObject.h"
 
 int User::signUp(string phone, string passwd, string userType) {
-    ifstream connectDB("connectDB.txt", ios::in);  //从文件读取数据库登入信息
+    ifstream connectDB("connectDB.in", ios::in);  //从文件读取数据库登入信息
     string argv[10];
     int argc = 0;
 

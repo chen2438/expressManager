c92b0b5d3e3c640425cf8b846b1956c780db41aa
chen2438 <2363822647@qq.com>
2022-11-26 11:58:46 +0800

    Update login.php

--- login/login.php
+++ login/login.php
@@ -1,14 +1,15 @@
 <?php
-$file = fopen("login.txt", "w+");
+$file = fopen("login.txt", "w+") or die("Unable to open file!");
 $user_name = $_POST['user_name'];
 $user_phone = $_POST['user_phone'];
 $values = $user_name . ' ' . $user_phone;
 if (empty($values)) {
     echo "values is empty";
+    fwrite($file, "values is empty");
 } else {
     $command = "./login " . escapeshellcmd($values);
     exec($command, $output);
-    echo $output;
+    fwrite($file, $output);
 }
 fclose($file);
 ?>

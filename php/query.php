4bf85406fbface2c0f1f70e1f3f6dacb0dee2d47
chen2438 <2363822647@qq.com>
2022-12-03 17:13:58 +0800

    Update query.php

--- php/query.php
+++ php/query.php
@@ -6,21 +6,33 @@ for ($i = 0; $i < 3; $i++) {
     $argv = $argv . ' ' . $post;
 }
 
-// echo "PHP: Get POST infomation";
-// echo '<br>';
+echo "PHP: Get POST infomation";
+echo '<br>';
 
 $command = "../cpp/main " . escapeshellcmd($argv);
 $output = array();
 
-// echo "PHP: exec " . $command;
-// echo '<br>';
+echo "PHP: exec " . $command;
+echo '<br>';
 
 exec($command, $output);
 
+$label = 0;
 for ($i = 0; $i < count($output); $i++) {
-    // echo 'C++: ' . $output[$i];
-    echo $output[$i];
-    // echo '<br>';
+    if ($output[$i] == "Return for PHP begin:") {
+        $label = 1;
+        continue;
+    }
+    if ($output[$i] == "Return for PHP end.") {
+        $label = 0;
+        continue;
+    }
+    if ($label == 0) {
+        echo 'C++: ' . $output[$i];
+        echo '<br>';
+    } else {
+        echo $output[$i];
+    }
 }
 
 // header("refresh: 3;url=../index.html");

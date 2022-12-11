7903857ca9502502fb220b8f09edffafb153f9ad
chen2438 <2363822647@qq.com>
2022-12-11 16:36:15 +0800

    Update main.cpp

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -3,6 +3,10 @@
 const char* Collector::adminPasswd = "admin";
 
 int main(int argc, char* argv[]) {  // 系统界面传参
+    Recipient recipient;
+    Collector collector;
+    User user;
+    ExpressManager manager;
     if (argc == 1) {
         exit(0);
     } else if (strcmp(argv[1], "signUp") == 0) {  // 注册
@@ -10,13 +14,11 @@ int main(int argc, char* argv[]) {  // 系统界面传参
         cout << "Start registration." << endl;
         if (strcmp(argv[4], "0") == 0) {
             cout << "Register for recipient." << endl;
-            Recipient recipient;
             argv[4] = (char*)"recipient";
             recipient.signUp(argv + 2);
             cout << "Registration successful!" << endl;
         } else if (strcmp(argv[4], Collector::adminPasswd) == 0) {
             cout << "Password correct. Register for collector." << endl;
-            Collector collector;
             argv[4] = (char*)"collector";
             collector.signUp(argv + 2);
             cout << "Registration successful!" << endl;
@@ -26,7 +28,6 @@ int main(int argc, char* argv[]) {  // 系统界面传参
         }
     } else if (strcmp(argv[1], "logIn") == 0) {  // 登录
         cout << "Start login." << endl;
-        User user;
         int res = user.logIn(argv + 2);
         if (res == 1) {
             cout << "Login successful!" << endl;
@@ -39,28 +40,23 @@ int main(int argc, char* argv[]) {  // 系统界面传参
         }
     } else if (strcmp(argv[1], "record") == 0) {  // 录入
         cout << "Start recording." << endl;
-        ExpressManager manager;
         manager.record(argv + 2);
         cout << "Recording successful!" << endl;
     } else if (strcmp(argv[1], "query") == 0) {  // 查询
         cout << "Start querying." << endl;
-        ExpressManager manager;
         argv[5] = (char*)"null";
         manager.query(argv + 2);
         cout << "Querying successful!" << endl;
     } else if (strcmp(argv[1], "delete") == 0) {  // 删除
         cout << "Start deleting." << endl;
-        ExpressManager manager;
         manager.del(argv + 2);
         cout << "Delete successful!" << endl;
     } else if (strcmp(argv[1], "queryAll") == 0) {  // 查询所有未取
         cout << "Start querying." << endl;
-        ExpressManager manager;
         manager.queryAll();
         cout << "Querying successful!" << endl;
     } else if (strcmp(argv[1], "mark") == 0) {  // 标记取件
         cout << "Start marking." << endl;
-        ExpressManager manager;
         manager.mark(argv + 2);
         cout << "Mark successful!" << endl;
     }

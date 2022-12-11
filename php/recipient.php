77cf25327fbb53973ee6dd4ee3210be4e05415b2
chen2438 <2363822647@qq.com>
2022-12-11 21:13:22 +0800

    upd

---
+++ php/recipient.php
@@ -0,0 +1,29 @@
+<?php
+
+$argv = "query null null " + $phone + " null";
+// expressID, pickupID, phone, picked
+$command = "../cpp/main " . escapeshellcmd($argv);
+
+echo "PHP: exec " . $command;
+echo '<br>';
+$output = array();
+exec($command, $output);
+
+// 输出查询结果
+
+$label = 0;
+for ($i = 0; $i < count($output); $i++) {
+    if ($output[$i] == "Return for PHP begin:") {
+        $label = 1;
+        continue;
+    } else if ($output[$i] == "Return for PHP end.") {
+        $label = 0;
+        continue;
+    }
+    if ($label == 0) {
+        echo 'C++: ' . $output[$i];
+        echo '<br>';
+    } else {
+        echo $output[$i];
+    }
+}

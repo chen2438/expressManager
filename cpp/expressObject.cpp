c1ce22baeb5e0dbaca043f88fdd25c7aa12e9428
chen2438 <2363822647@qq.com>
2022-12-11 20:41:32 +0800

    Update expressObject.cpp

--- cpp/expressObject.cpp
+++ cpp/expressObject.cpp
@@ -166,6 +166,13 @@ void ExpressManager::stats(char* argv[]) {  // 信息统计
     res = db.exeSQL("select * from express;");
     int sumIn, sumOut;
     map<string, int> companyIn, companyOut, companyNoPickup;
+    for (auto i : res) {
+        for (auto j : i) {
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+    return;
     for (auto row : res) {  // 统计当日总收取和各公司收取
         echo(row[15]);
         echo(Date);
